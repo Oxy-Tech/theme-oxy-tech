9d4760be2607dd20fc28cd5c313aecaedf131904
etiennezastko <etiennezastko@hotmail.fr>
2021-07-05 18:16:19 +0200

    theme-wordpress-oxy-tech

---
+++ header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="<?php bloginfo('description'); ?>">
+  <title><?php bloginfo('name'); ?></title>
+  <?php wp_head(); ?>
+
+
+
+</head>
+
+<body><?php /* body_class(); */ ?>
+  <?php wp_body_open(); ?>
+
+
+
+  <main class="container">
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <?= get_custom_logo($blog_id) ?>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <?php wp_nav_menu('theme_location => main'); ?>
+
+
+
+            <?php
+            wp_nav_menu(array(
+              'theme_location'    => 'primary',
+              'depth'             => 2,
+              'container'         => 'div',
+              'container_class'   => 'collapse navbar-collapse',
+              'container_id'      => 'bs-example-navbar-collapse-1',
+              'menu_class'        => 'nav navbar-nav',
+              'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+              'walker'            => new WP_Bootstrap_Navwalker(),
+            ));
+            ?>
+        </div>
+    </nav>
+
+    <!-- <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li> -->
+    </ul>
+    <div class="d-flex">
+      <button id="btn-kontakt" class="btn btn-outline-success" type="submit">Search</button>
+
+
+    </div>
+    </div>
+    </div>
+    </nav>
---
+++ index.php
@@ -0,0 +1,183 @@
+<?php include "header.php"; ?>
+
+<!--  -->
+<!-- Première section -->
+<div class="card mb-3 guideline" style="max-width: 1296px;">
+    <div class="row g-0 guideline">
+        <div class="col-md-6 guideline">
+            <div class="card-body guideline">
+                <p class="subtitle guideline">INGENIEUR DIETER WEISS </p>
+
+                <h2 class="card-title guideline">
+                    Ingenieurbûro fûr Bau- und Vermessungswesen</h2>
+
+                <p class="card-text-1 guideline">Wenn Sie zuverlâssige Ingenieure brauchen- das
+                    Ingenieurbûro Dieter Weiss ist Garant fûr den Erlog Ihrer
+                    Projekte</p>
+
+                <button class="btn-btn-primary section_button-1" type="submit">Ihr Projekt mituns</button>
+            </div>
+        </div>
+        <div class="col-md-6 image-align guideline">
+
+            <img src="
+<?php echo get_template_directory_uri(); ?>/images/image-weiss-version-desktop/image-1.png" alt="image-1.png">
+
+        </div>
+    </div>
+</div>
+
+<!-- Deuxième section -->
+<div class="card mb-3 guideline" style="max-width: 1296px;">
+    <div class="row g-0">
+        <div class="col-md-6">
+            <div class="card-body guideline">
+                <p class="subtitle guideline">INGENIEUR DIETER WEISS </p>
+                <h2 class="card-title guideline">
+                    Ingenieurbûro fûr Bau- und Vermessungswesen</h2>
+                <p class="card-text-1 guideline">Wenn Sie zuverlâssige Ingenieure brauchen- das
+                    Ingenieurbûro Dieter Weiss ist Garant fûr den Erlog Ihrer
+                    Projekte</p>
+
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Troisième section -->
+
+    <div class="row guideline">
+        <div class="col-sm-12 col-md-4 guideline">
+            <div class="card">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/image-weiss-version-desktop/article-1.png" class="article-1.png" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Verkehrsanlagen und Ingernieurbauwerke</h5>
+                    <p class="card-text">Neubau und Ausbau vo, Straßen,
+                        Verkehrsflâchen, Wasserwergen und Eisenvahnen erfordert ûberzeugende Verkehrskonzepte.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-4 guideline">
+            <div class="card">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/image-weiss-version-desktop/article-2.png" class="article-2.png" alt="articles-2">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        Studien und Konzepte, Gesamtplanung
+                        Vermessungswesen</h5>
+                    <p class="card-text">Verkehrskonzepte sind ein entscheide
+                        Faktor jeglischer Infrastruktur. Das Ingenieurbûro Weiss hat deshalb ein Expertenteam aufgebaut</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-4 guideline">
+            <div class="card guideline">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/image-weiss-version-desktop/article-3.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Entwurfsvermessun, 3D-Laserscanning</h5>
+                    <p class="card-text">Bei Ausfûhrung sâmtlicher
+                        vermessungtechischer Aufgaben im Leistungsbild,, Entwurfsvermessung” sind wir ein leistungsstarker Partner.</p>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Quatrième section -->
+        <div class="col-sm-12 col-md-10 guideline">
+            <div class="parent guideline" id="margin-card">
+                <div class="child guideline">
+                    <p class="card-subtitle guideline">UNSERE
+                        FIRMENHISTORIE</p>
+                    <h5 class="card-title-2 child guideline">Diplom-Ingenieur Dieter Weiss grûndete im Jahre 1971 sein Ingenieurnûro.
+                    </h5>
+                    <p class="card-text-2 guideline">
+                        Fast 30 Jahre lang fûhrte er es als Alleinninhaber. 1998 trat Sohn Klaus weiss als Stellvertreter an die Seite seines Vaters. Seit 2006 ist Diplom-Ingenieur Klauss Weiss alleiniger Gesvhâftsfûhrer.</p>
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/image-weiss-version-desktop/image-3.png" alt="" class="test">
+                </div>
+                <div class="child">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/image-weiss-version-desktop/vector.png" alt="">
+                </div>
+
+            </div>
+            <!--Cinquieme Section -->
+            <div class="card-text-end pt-5 pb-5 guideline ">
+                <div class="card-body-9 card-align d-flex justify-content-between align-items-end card-media">
+
+                    <div class="margin-card-2 guideline card-full-2">
+                        <h5 class="card-title">Winssenswertes </h5>
+                        <p class="card-text-5">
+                            Winssenswertes
+                            Integer massa urna senectus maecenas in dapibus urna amet henderitt. In Ipsum, morbi amet aenan. laculis donec sem nullam nunc condimentum nisl non gravida in.
+
+                        </p>
+                    </div>
+                    <div>
+                        <button type="button" class="btn btn-warning section-5__button">Unser Blog</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <!--Sixième Section -->
+        <div class="row guideline">
+            <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title section-6-title">Sed eget morbi cursus elit ipsum pellentesque</h5>
+                        <p class="card-text-6">Pellentesque mauris arcu nec, Duis sebectus cras semper cursus bibendum phasellus vehicula pottitor...</p>
+                        <button type="button" class="btn btn-link link-left ">Lesen Sie weiter ></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title section-6-title">Urna velit semper faucibus ac suspendisse et</h5>
+                        <p class="card-text-6">In nibh solales ticindut vitae tortor. Turpis aliquam mattis eget cursus billendum interdum vestibulum, aliquam Fringilla malesuada</p>
+                        <button type="button" class="btn btn-link link-left ">Lesen Sie weiter ></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title section-6-title">Bilbendum augue imperdiet sapien quis enim elementum</h5>
+                        <p class="card-text-6">Facilis id mus at pulvinar vitae, ornare vitae scelerisque. Lacus egestas augue cursus aliquam nulla. Sac erat a scelerisque neque..</p>
+                        <button type="button" class="btn btn-link link-left ">Lesen Sie weiter ></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card-text-end pt-5 pb-5 guideline ">
+            <div class="card-body-9 card-align d-flex justify-content-between align-items-end card-media">
+
+                <!--7ème section-->
+                <div class=" guideline card-full-3 autre">
+                    <div class="text">
+                        <p class="subtitle_section-7">
+                            MACH KARRIERE BEI UNIS</p>
+                        <h5 class="card-title">Bewirb dich bei uns </h5>
+                        <p class="card-text-5">
+                            Lorem ipsum dolor sit amet, consecteur adipiscing elit,sed do eiusmod tempor incididunt ut labore el dolor
+                    </div>
+                    <div class="row">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/image-weiss-version-desktop/image.png" alt="" class="image">
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+            <!-- </div>
+</div> -->
+            <?php include "footer.php"; ?>
